71bd8574772cecf873c9badb696eda56f4d8b201
Myles Braithwaite <me@mylesbraithwaite.com>
2011-10-04 13:03:05 -0400

    Title casesing the passwords.

--- bin/xkcd-passwd.py
+++ bin/xkcd-passwd.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 from random import sample
-print ' '.join(sample(open('/usr/share/dict/words', 'r').read().split('\n'), 3))
+print ' '.join(sample(open('/usr/share/dict/words', 'r').read().split('\n'), 3)).title()
