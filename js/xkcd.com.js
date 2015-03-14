78dde52887ba26e4973e657d40a709dcd674860f
Myles Braithwaite <me@mylesbraithwaite.com>
2015-03-13 23:42:20 -0400

    Not using dotjs anymore so I just delete everything.

--- js/xkcd.com.js
+++
@@ -1,8 +0,0 @@
-var comic = $("div.s img[title]");
-var titleP = $("<p></p>");
-titleP.css({"font-variant": "normal",
-	    "border": "1px solid yellow",
-	    "padding": "1em",
-	    "background-color": "#FFFFCC"});
-titleP.text(comic.attr("title"));
-comic.after(titleP);
