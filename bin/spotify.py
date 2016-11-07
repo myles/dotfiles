aeb70a9b224afeb3c35074fc4593b4c55114f841
Myles Braithwaite <me@mylesbraithwaite.com>
2016-11-07 15:07:48 -0500

    Fixed up my Spotify control.

--- bin/spotify.py
+++ bin/spotify.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import sys
+import logging
 from subprocess import run, PIPE
-from argparse import ArgumentParser, REMAINDER
+from argparse import ArgumentParser
 
 
 class SpotifyControl(object):
@@ -11,25 +13,17 @@ class SpotifyControl(object):
         self.osascript_path = osascript_path
 
     def osascript(self, script):
-        """
-        This is a bridge between Python adn AppleScript using the command line
-        application `osascript`.
-        """
+        logging.debug('Running `{}`.'.format(script))
+
         process = run([self.osascript_path, '-e', script],
                       stdout=PIPE)
         return process.stdout.decode('utf-8').strip()
 
     def spotify(self, arg):
-        """
-        This is so I don't have to repeat a bunch of code.
-        """
         script = 'tell application "{0}" to {1} as string'
         return self.osascript(script.format(self.app, arg))
 
     def is_running(self):
-        """
-        Check to see if Spotify is currently running.
-        """
         command = 'application "{0}" is running'
         output = self.osascript(command.format(self.app))
 
@@ -38,64 +32,13 @@ class SpotifyControl(object):
         else:
             return False
 
-    def activate(self):
-        """
-        Start the Spotify application.
-        """
-        return self.spotify('activate')
-
-    def quit(self):
-        """
-        Quit the Spotify application.
-        """
-        return self.spotify('quit')
-
-    def player_state(self):
-        """
-        Check to see the player state.
-        """
-        return self.spotify('player state')
-
-    def play(self):
-        """
-        Start playing music.
-        """
-        return self.spotify('play')
-
-    def pause(self):
-        """
-        Pause the music.
-        """
-        return self.spotify('pause')
-
-    def playpause(self):
-        """
-        Toggle play/pause.
-        """
-        return self.spotify('playpause')
-
-    def next_track(self):
-        """
-        Play the next track.
-        """
-        return self.spotify('next track')
-
-    def previous_track(self):
-        """
-        Play the previous track.
-        """
-        return self.spotify('previous track')
-
     def play_track(self, uri):
-        """
-        Start playback of a track in the given context.
-        """
         return self.spotify('play track "{0}"'.format(uri))
 
+    def state(self):
+        return self.spotify('player state')
+
     def current_track(self):
-        """
-        Get some meta data from the currently playing track.
-        """
         data = {}
         data['artist'] = self.spotify('artist of current track')
         data['album'] = self.spotify('album of current track')
@@ -104,48 +47,110 @@ class SpotifyControl(object):
         data['artwork_url'] = self.spotify('artwork url of current track')
         return data
 
+    def command_on(self):
+        if self.is_running():
+            print('Spotify is already on.')
+        else:
+            self.spotify('activate')
+
+    def command_off(self):
+        if not self.is_running():
+            print('Spotify is already off.')
+        else:
+            self.spotify('quit')
+
+    def command_state(self):
+        if self.is_running():
+            print('{}'.format(self.state()))
+        else:
+            print('Spotify is not running.')
+
+    def command_current(self):
+        if self.is_running():
+            print('{name} / {artist}'.format(**self.current_track()))
+        else:
+            print('Spotify is not running.')
+
+    def command_play(self):
+        if self.is_running():
+            self.spotify('play')
+            self.command_current()
+        else:
+            print('Spotify is not running.')
+
+    def command_pause(self):
+        if self.is_running():
+            self.spotify('pause')
+        else:
+            print('Spotify is not running.')
+
+    def command_toggle(self):
+        if self.is_running():
+            self.spotify('playpause')
+            if self.state() == 'paused':
+                self.command_state()
+            elif self.state() == 'playing':
+                self.command_current()
+        else:
+            print('Spotify is not running.')
+
+    def command_next(self):
+        if self.is_running():
+            self.spotify('next track')
+            self.command_current()
+        else:
+            print('Spotify is not running.')
+
+    def command_prev(self):
+        if self.is_running():
+            self.spotify('previous track')
+            self.command_current()
+        else:
+            print('Spotify is not running.')
+
 
 def main():
+    spotify = SpotifyControl()
+
     parser = ArgumentParser()
-    parser.add_argument('command', nargs=REMAINDER)
+
+    subparsers = parser.add_subparsers(title='commands')
+
+    on_parser = subparsers.add_parser('on', help='Open the Spotify '
+                                                 'application.')
+    on_parser.set_defaults(func=spotify.command_on)
+
+    off_parser = subparsers.add_parser('off', help='Close the Sptoify '
+                                                   'application.')
+    off_parser.set_defaults(func=spotify.command_off)
+
+    state_parser = subparsers.add_parser('state', help='Return the current '
+                                                       'state of the Spotify '
+                                                       'player.')
+    state_parser.set_defaults(func=spotify.command_state)
+
+    play_parser = subparsers.add_parser('play', help='Play the music.')
+    play_parser.set_defaults(func=spotify.command_play)
+
+    pause_parser = subparsers.add_parser('pause', help='Pasue the music.')
+    pause_parser.set_defaults(func=spotify.command_pause)
+
+    toggle_parser = subparsers.add_parser('toggle', help='Play/Pause the '
+                                                         'music.')
+    toggle_parser.set_defaults(func=spotify.command_toggle)
+
+    next_parser = subparsers.add_parser('next')
+    next_parser.set_defaults(func=spotify.command_next)
+
+    previous_parser = subparsers.add_parser('previous')
+    previous_parser.set_defaults(func=spotify.command_prev)
+
+    current_parser = subparsers.add_parser('current', help='Get the current '
+                                                           'song playing.')
+    current_parser.set_defaults(func=spotify.command_current)
+
     args = parser.parse_args()
-
-    s = SpotifyControl()
-
-    if 'on' in args.command:
-        if not s.is_running():
-            s.activate()
-        else:
-            print('Spotify is already running.')
-
-    if 'off' in args.command:
-        if s.is_running():
-            s.quit()
-        else:
-            print('Sptoify is already not running.')
-
-    if 'state' in args.command:
-        print('Sptoify is in the state of "{}".'.format(s.player_state()))
-
-    if 'play' in args.command:
-        s.play()
-
-    if 'pause' in args.command:
-        s.pause()
-
-    if 'toggle' in args.command:
-        s.playpause()
-
-    if 'next' in args.command:
-        s.next_track()
-        print('Playing: {name} by {artist}'.format(**s.current_track()))
-
-    if 'previous' in args.command:
-        s.previous_track()
-        print('Playing: {name} by {artist}'.format(**s.current_track()))
-
-    if 'current' in args.command:
-        print('Playing: {name} by {artist}'.format(**s.current_track()))
+    args.func()
 
 
 if __name__ == '__main__':
