50373a17b73edf91ef0e6876f65e8c856aadc27f
Myles Braithwaite <me@mylesbraithwaite.com>
2017-07-31 12:58:12 -0400

    Fix a minor Fish config error.

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -1,13 +1,13 @@
-test -d "$HOME/.bin";         and set PATH "$HOME/.bin" $PATH
-test -d "$HOME/.bin-local";   and set PATH "$HOME/.bin-local" $PATH
-test -d "$HOME/.bin-private"; and set PATH "$HOME/.bin-private" $PATH
+test -d "$HOME/.bin"; and set -g fish_user_paths "$HOME/.bin" $fish_user_paths
+test -d "$HOME/.bin-local"; and set -g fish_user_paths "$HOME/.bin-local" $fish_user_paths
+test -d "$HOME/.bin-private"; and set -g fish_user_paths "$HOME/.bin-private" $fish_user_paths
 
-test -d "$GOPATH/bin";        and set PATH "$GOPATH/bin" $PATH
+test -d "$GOPATH/bin"; and set -g fish_user_paths "$GOPATH/bin" $fish_user_paths
 
-test -d "$HOME/.rbenv/bin";   and set PATH "$HOME/.rbenv/bin" $PATH
-test -d "$HOME/.rbenv/shims"; and set PATH "$HOME/.rbenv/shims" $PATH
+test -d "$HOME/.rbenv/bin"; and set -g fish_user_paths "$HOME/.rbenv/bin" $fish_user_paths
+test -d "$HOME/.rbenv/shims"; and set -g fish_user_paths "$HOME/.rbenv/shims" $fish_user_paths
 
-test -d "/usr/local/sbin";    and set PATH "/usr/local/sbin" $PATH
-test -d "/usr/local/bin";     and set PATH "/usr/local/bin" $PATH
+test -d "/usr/local/sbin"; and set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+test -d "/usr/local/bin"; and set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 
-set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+test -d "/usr/local/opt/gettext/bin"; and set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
